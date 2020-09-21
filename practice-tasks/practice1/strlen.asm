4a9352bc8d6abe7586f9c087babd10ea748b7e76
Emma suncong_xdu <sunonion2001@163.com>
2020-09-21 22:47:43 +0800

    chap03 and practice1

---
+++ practice-tasks/practice1/strlen.asm
@@ -0,0 +1,28 @@
+.386
+.model flat, stdcall
+
+include kernel32.inc
+includelib kernel32.lib
+
+include msvcrt.inc
+includelib msvcrt.lib
+
+.data
+szText	db	"Reverse Engineering", 0
+format	db	"length = %d", 0AH, 0
+
+.code
+
+main PROC
+	LEA EDI, szText
+	MOV ECX,0FFFFFFFFH
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;strlenÂß¼­
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	INVOKE crt_printf, addr format, EDI
+
+	INVOKE crt_getchar
+	INVOKE ExitProcess, 0
+main ENDP
+
+END main
